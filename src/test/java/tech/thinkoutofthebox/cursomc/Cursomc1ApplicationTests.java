bf193a93d424d54d309323766a4c6ecd9d4f8ab5
Renatol1857 <renatol1857@gmail.com>
2021-06-22 14:11:38 -0300

    Projeto Criado

---
+++ src/test/java/tech/thinkoutofthebox/cursomc/Cursomc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.thinkoutofthebox.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
